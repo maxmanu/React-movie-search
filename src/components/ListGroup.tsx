ccc2138e03a2818af3e0a7a9007983a0311e1221
max <maxcamina@gmail.com>
2023-03-18 09:55:13 -0500

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+function ListGroup() {
+    let items = ["New York", "San Francisco", "Tokio", "London", "Paris"];
+
+    return (
+        <>
+            <h1>List</h1>
+            {items.length === 0 && <p>No item found</p>}
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li key={item} className="list-group-item">
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
